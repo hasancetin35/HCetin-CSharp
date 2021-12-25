1ebca0ac1715310c55e367a116ae3db663908cd2
hasancetin35 <hasancetin35@gmail.com>
2021-12-26 00:55:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HCetin_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
