a1fea5055044a05ee5253b6b7ef9bf914db10b63
hasancetin35 <hasancetin35@gmail.com>
2021-12-29 00:03:46 +0300

    hata yakalama

--- Odevler.cs
+++ Odevler.cs
@@ -132,6 +132,31 @@ namespace HCetin_CSharp
         }
 
 
+        public void hatayakalama() {
+
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+        }
+
+
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,6 +17,9 @@ namespace HCetin_CSharp
             nesne.Operatorler();
 
             nesne.tip_donusum();
+
+
+            nesne.hatayakalama();
         }
     }
 }
