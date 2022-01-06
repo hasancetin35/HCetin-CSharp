784209e20104c47b7e2af7ce4c8e1fe4a4435ce4
hasancetin35 <hasancetin35@gmail.com>
2022-01-07 01:42:47 +0300

    struct

--- SinifOrnek.cs
+++ SinifOrnek.cs
@@ -51,4 +51,55 @@ namespace HCetin_CSharp
 
         }
     }
+
+    class Calisan
+    {
+
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;  }
+   
+
+        private string isim;
+
+        private string soyisim;
+
+
+        public Calisan(string Isim,string Soyisim)
+        {
+
+            isim = Isim;
+            soyisim = Soyisim;
+            calisanSayisi++;
+
+        }
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+    }
+
+
+
+    struct Dikdorgen_Struct
+
+    {
+        public int kisakenar;
+
+        public int uzunkenar;
+
+
+        public long Alanhesapla()
+        {
+
+            return this.kisakenar * this.uzunkenar;
+
+
+        }
+
+
+    }
+
+
 }
